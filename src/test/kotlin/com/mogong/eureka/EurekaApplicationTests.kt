6f6d0ae3bc388611cc9aa0865bbae6f24b0ded24
jonghyukleee <jh5253krkr@naver.com>
2023-08-08 23:27:58 +0900

    init project!

---
+++ src/test/kotlin/com/mogong/eureka/EurekaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mogong.eureka
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
